01cfa7c8b22346ba238551e4e240782f1ab0d3cc
nekola <nekolacats@gmail.com>
2025-03-08 11:19:30 -0300

    mais outros ai

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+// Teste no Final
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+// Teste no Inicio
+/*
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+} 
+*/
